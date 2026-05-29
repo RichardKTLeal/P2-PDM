5581986a23741a37c695e83cee19b3daf357acbc
richard kazuo takada leal <richardktleal@gmail.com>
2026-05-29 13:31:39 -0300

    feat(p2): Criação da interface de busca por nome do país

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,64 +1,40 @@
-import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  ScrollView,
-} from "react-native";
-import { TextInput, Button } from "react-native";
 import { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet, ScrollView } from "react-native";
 
-const [nome, setNome] = useState("");
+export default function Index() {
+  const [nome, setNome] = useState("");
 
-export default function App() {
   return (
     <ScrollView style={styles.container}>
-      <Text style={styles.titulo}>
-        Informações sobre Países
-      </Text>
+      <Text style={styles.titulo}>Informações sobre Países</Text>
+
       <View style={styles.card}>
-  <Text>Buscar por nome do país</Text>
+        <Text style={styles.subtitulo}>Buscar por nome do país</Text>
 
-  <TextInput
-    style={styles.input}
-    placeholder="Ex: Brazil"
-    value={nome}
-    onChangeText={setNome}
-  />
+        <TextInput
+          style={styles.input}
+          placeholder="Ex: Brazil"
+          value={nome}
+          onChangeText={setNome}
+        />
 
-  <Button
-    title="Buscar país"
-    onPress={() => {}}
-  />
-</View>
+        <Button title="Buscar país" onPress={() => {}} />
+      </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: "#f2f2f2",
+  container: { flex: 1, padding: 20, backgroundColor: "#f2f2f2" },
+  titulo: { fontSize: 26, fontWeight: "bold", textAlign: "center", marginBottom: 20 },
+  card: { backgroundColor: "#fff", padding: 15, borderRadius: 10, marginBottom: 20 },
+  subtitulo: { fontSize: 20, fontWeight: "bold", marginBottom: 10 },
+  input: {
+    borderWidth: 1,
+    borderColor: "#aaa",
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 10,
+    backgroundColor: "#fff",
   },
-
-  titulo: {
-    fontSize: 26,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 20,
-  },
-  card: {
-  backgroundColor: "#fff",
-  padding: 15,
-  borderRadius: 10,
-},
-
-input: {
-  borderWidth: 1,
-  borderColor: "#aaa",
-  borderRadius: 8,
-  padding: 10,
-  marginBottom: 10,
-},
 });
